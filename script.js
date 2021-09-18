be5c307dc19fdc70673596d4e694525eae4357cf
Brian Bacik <bdbacik@gmail.com>
2021-09-18 18:27:46 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
 
     // display result when compute interest is clicked
-    document.getElementById("result").textContent="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
 
 }
   
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText='${rateval}%';
 }
 
 function checkinput()
